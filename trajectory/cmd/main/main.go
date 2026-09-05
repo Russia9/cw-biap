0ef76468a020e89aa8d07d0822965a01bf8592cd
Andrey Kovalev <russia9@russia9.dev>
2026-09-06 01:10:18 +0300

    output trajectory

--- trajectory/cmd/main/main.go
+++ trajectory/cmd/main/main.go
@@ -7,23 +7,20 @@ import (
 	"traj/aero"
 
 	na "github.com/Russia9/numerical-analysis"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
-	"gonum.org/v1/plot/vg"
 )
 
 const h = 0.1
 
 // args[1] = rocket.json path
 // args[2] = aero averages.csv path
+// args[3] = output.csv path
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 4 {
 		panic("wrong number of arguments")
 	}
 
-	// Loading rocket.json
-	rocket, err := traj.LoadRocketJSON(os.Args[1])
+	// Loading r.json
+	r, err := traj.LoadRocketJSON(os.Args[1])
 	if err != nil {
 		panic(fmt.Errorf("rocket.json: %w", err))
 	}
@@ -39,9 +36,9 @@ func main() {
 	}
 
 	// Init model
-	model := traj.InitModel(rocket, aero)
+	model := traj.InitModel(r, aero)
 	tChar := make([]float64, 0)
-	for i, st := range rocket.Stages {
+	for i, st := range r.Stages {
 		if i > 0 {
 			tChar = append(tChar, tChar[i-1]+st.BurnTime)
 		} else {
@@ -65,7 +62,7 @@ func main() {
 	}
 
 	// Visualize the trajectory
-	{
+	/*{
 		p := plot.New()
 
 		p.Title.Text = "Trajectory"
@@ -85,5 +82,46 @@ func main() {
 		if err := p.Save(15*vg.Inch, 8*vg.Inch, "out/trajectory.png"); err != nil {
 			panic(err)
 		}
+	}*/
+
+	// Write the resulting CSV
+	out, err := os.Create(os.Args[3])
+	if err != nil {
+		panic(fmt.Errorf("out file: %w", err))
+	}
+	defer out.Close()
+
+	out.WriteString("t,m,x,y,Vx,Vy,V,r,h,pitch,flightAngle,attack\n")
+	for i := range res[0] {
+		t := res[0][i].X                   // Current time
+		stI := traj.StageIndex(r, true, t) // Stage index
+		st := r.Stages[stI]                // Stage
+
+		// State slice
+		y := make([]float64, len(res))
+		for j := range res {
+			y[j] = res[j][i].Y
+		}
+
+		// pitch and attack
+		pitch := r.Pitch.Pitch(t)
+		attack := r.Pitch.Pitch(t) - traj.FlightAngle(y...)
+		if !st.Controlled {
+			pitch = traj.FlightAngle(y...)
+			attack = 0
+		}
+
+		fmt.Fprintf(out, "%.3f,%e,%e,%e,%e,%e,%e,%e,%e,%e,%e,%e\n",
+			t,
+			traj.Mass(st, t, traj.StageT0(r, stI)),
+			res[traj.IX][i].Y, res[traj.IY][i].Y,
+			res[traj.IVx][i].Y, res[traj.IVy][i].Y,
+			traj.VelMag(y...),
+			traj.RadiusVec(y...),
+			traj.Altitude(y...),
+			pitch,
+			traj.FlightAngle(y...),
+			attack,
+		)
 	}
 }
--- trajectory/model.go
+++ trajectory/model.go
@@ -10,33 +10,38 @@ import (
 
 // State layout
 const (
-	iVx = 0
-	iVy = 1
-	iX  = 2
-	iY  = 3
+	IVx = 0
+	IVy = 1
+	IX  = 2
+	IY  = 3
 )
 
+// Radius-vector R
+func RadiusVec(y ...float64) float64 {
+	return math.Hypot(y[IX], Rz+y[IY])
+}
+
 // Altitude H = R − Rz [m].
-func Altitude(y ...float64) float64 { return math.Hypot(y[iX], Rz+y[iY]) - Rz }
+func Altitude(y ...float64) float64 { return RadiusVec(y...) - Rz }
 
 // VelMag returns |V| [m/s].
-func VelMag(y ...float64) float64 { return math.Hypot(y[iVx], y[iVy]) }
+func VelMag(y ...float64) float64 { return math.Hypot(y[IVx], y[IVy]) }
 
 // FlightAngle theta = atan2(Vy, Vx) [rad].
-func FlightAngle(y ...float64) float64 { return math.Atan2(y[iVy], y[iVx]) }
+func FlightAngle(y ...float64) float64 { return math.Atan2(y[IVy], y[IVx]) }
 
 // gravity components of the central field
 func gravity(y ...float64) (gx, gy float64) {
-	r := math.Hypot(y[iX], Rz+y[iY])
+	r := RadiusVec(y...)
 	r3 := r * r * r
-	gx = -MuZ * y[iX] / r3
-	gy = -MuZ * (Rz + y[iY]) / r3
+	gx = -MuZ * y[IX] / r3
+	gy = -MuZ * (Rz + y[IY]) / r3
 	return gx, gy
 }
 
-// stage determines current stage from time.
-// final stage is carried on regardless of BurnTime.
-func stage(r Rocket, fromRight bool, t float64) int {
+// StageIndex determines current StageIndex from time.
+// final Stage is carried on regardless of BurnTime.
+func StageIndex(r Rocket, fromRight bool, t float64) int {
 	time := 0.
 	for i, st := range r.Stages {
 		if Eq(time+st.BurnTime, t, 1e-10) { // if we are at the stage separation event
@@ -55,11 +60,21 @@ func stage(r Rocket, fromRight bool, t float64) int {
 	return len(r.Stages) - 1
 }
 
-func thrust(st Stage, p float64) float64 {
+func StageT0(r Rocket, stageIndex int) float64 {
+	t0 := 0.
+	for i, cur := range r.Stages {
+		if i < stageIndex {
+			t0 += cur.BurnTime
+		}
+	}
+	return t0
+}
+
+func Thrust(st Stage, p float64) float64 {
 	return ((st.ISpSurface-st.ISpVacuum)/P0*p + st.ISpVacuum) * st.MassFlow() * G0
 }
 
-func mass(st Stage, t, t0 float64) float64 {
+func Mass(st Stage, t, t0 float64) float64 {
 	if st.Powered {
 		return st.M0 - st.MassFlow()*(t-t0)
 	}
@@ -67,19 +82,14 @@ func mass(st Stage, t, t0 float64) float64 {
 }
 
 func accel(r Rocket, aero map[string]*aero.Aero, fromRight bool, t float64, y ...float64) (ax, ay float64) {
-	stI := stage(r, fromRight, t)
+	stI := StageIndex(r, fromRight, t)
 	st := r.Stages[stI]
 
 	// calculate stage start time
-	t0 := 0.
-	for i, cur := range r.Stages {
-		if i < stI {
-			t0 += cur.BurnTime
-		}
-	}
+	t0 := StageT0(r, stI)
 
 	// mass
-	m := mass(st, t, t0)
+	m := Mass(st, t, t0)
 
 	// atmosphere
 	_, rho, p, _, _, a := atmosphere.Atmosphere(Altitude(y...))
@@ -87,7 +97,7 @@ func accel(r Rocket, aero map[string]*aero.Aero, fromRight bool, t float64, y ..
 	// thrust
 	if st.Powered {
 		pitch := r.Pitch.Pitch(t)
-		P := thrust(st, p)
+		P := Thrust(st, p)
 		ax += P * math.Cos(pitch) / m
 		ay += P * math.Sin(pitch) / m
 	}
@@ -129,7 +139,7 @@ func InitModel(r Rocket, aero map[string]*aero.Aero) na.FuncSystem {
 			_, ay := accel(r, aero, fromRight, t, y...)
 			return ay
 		}, // dVy/dt
-		func(_ bool, _ float64, y ...float64) float64 { return y[iVx] }, // dx/dt
-		func(_ bool, _ float64, y ...float64) float64 { return y[iVy] }, // dy/dt
+		func(_ bool, _ float64, y ...float64) float64 { return y[IVx] }, // dx/dt
+		func(_ bool, _ float64, y ...float64) float64 { return y[IVy] }, // dy/dt
 	}
 }
